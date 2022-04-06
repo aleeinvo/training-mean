60bcddc6e5c9025a434e995c2fa67a959fcdbba1
aleeinvo <ali.asghar@invozone.com>
2022-04-06 23:42:29 +0500

    js datatypes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-let dev = {
-    name: 'Alee',
-    intro: function() {
-        console.log('my name is ' + this.name);
-    }
-}
+let devName = "Alee";
+let isUp = false;
+let age = 28;
+let next = undefined;
+let prev = null;
 
-dev.intro();
+console.log(typeof(devName));
+console.log(typeof(isUp));
+console.log(typeof(age));
+console.log(typeof(next));
+console.log(typeof(prev));
