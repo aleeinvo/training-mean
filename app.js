21a74503ffc363ba7e621121a045e9956a623869
aleeinvo <ali.asghar@invozone.com>
2022-04-06 21:02:40 +0500

    code works with or without semicolons

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 console.log('Hi');
 
 
+console.log('this will work fine, witespaces and tabs don\'t effect code exxecution.')
 
-
-
-
-
-
-            console.log('this will work fine, witespaces and tabs don\'t effect code exxecution.')
+// It works with or without semicolons just fine
