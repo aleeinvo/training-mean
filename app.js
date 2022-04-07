ecc5f0a6a765f50636fc741fcefcf7ee94e710c8
aleeinvo <ali.asghar@invozone.com>
2022-04-07 22:19:30 +0500

    for in on object properties

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-outer:
-for(let x = 0;x <= 10; x++) {
-    inner:
-    for(let y =0;y <= 10; y++) {
-        console.log(y);
-        if(y == 5) {
-            console.log('---------------------------------');
-            break inner;
-        }
-    }
+let dev = {
+    name: 'Alee',
+    city: 'Lahore'
+};
+
+for(let prop in dev) {
+    console.log(`${prop}: ${dev[prop]}`);
 }
