db730c3d795161fdb2b02cb925195c832af308e7
aleeinvo <ali.asghar@invozone.com>
2022-04-07 14:58:21 +0500

    array destruct

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-let data = {
-    name: 'Alee Dhillon',
-    role: 'Software Developer`'
-}
+const people = [
+    {
+        name: 'Mike Smith',
+        family: {
+            mother: 'Jane Smith',
+            father: 'Harry Smith',
+            sister: 'Samantha Smith'
+        },
+        age: 35
+    },
+    {
+        name: 'Tom Jones',
+        family: {
+            mother: 'Norah Jones',
+            father: 'Richard Jones',
+            brother: 'Howard Jones'
+        },
+        age: 25
+    }
+];
 
-console.log(data);
+for(const {name: fullName, family: { sister: sisterName = null, ...restFamily}} of people) {
+    console.log(fullName, sisterName);
+    console.log(restFamily);
+}
