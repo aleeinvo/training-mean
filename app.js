992140614994d8075593050464dd8fafb3d0e153
aleeinvo <ali.asghar@invozone.com>
2022-04-07 17:48:51 +0500

    if else if statements

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let person = {
     fName: 'Alee',
-    age: 28
+    age: 16
 }
 
 if(person.age >= 18) {
     console.log('Adult');
+} else if(person.age >= 13) {
+    console.log('Teen');
 } else {
     console.log('Minor');
 }
