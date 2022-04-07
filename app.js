feea62b49ac99d64af95cbf5df937663604eb887
aleeinvo <ali.asghar@invozone.com>
2022-04-07 21:54:36 +0500

    label statement in loops

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+outer:
 for(let x = 0;x <= 10; x++) {
-    console.log(x);
+    inner:
+    for(let y =0;y <= 10; y++) {
+        console.log(y);
+        if(y == 5) {
+            console.log('---------------------------------');
+            break inner;
+        }
+    }
 }
