64e5b1bb8bb07d915c396f57b42fdbcb7ab38f34
aleeinvo <ali.asghar@invozone.com>
2022-04-08 19:46:41 +0500

    auto refresh after 1 second

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ let refresh = document.getElementById('refresh');
 refresh.addEventListener('click', () => {
     location.reload();
 })
+
+function refreshPage() {
+    location.reload();
+}
+
+let time = document.getElementById('time');
+
+time.innerText = (new Date).toISOString();
+
+setInterval(refreshPage, 1000);
