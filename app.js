d6bd6d7d743a9eb9261dea809d2839ee077e94e6
aleeinvo <ali.asghar@invozone.com>
2022-04-08 15:23:50 +0500

    call an event just once

--- app.js
+++ app.js
@@ -6,16 +6,12 @@ function random(number) {
     return Math.floor(Math.random() * (number + 1));
 }
 
-function changeBg(){
+function changeBg() {
     const randColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
 
     document.body.style.backgroundColor = randColor;
-
-    count++;
-
-    if(count >= 10) {
-        btn.removeEventListener('mouseover', changeBg);
-    }
 }
 
-btn.addEventListener('mouseover', changeBg);
+btn.addEventListener('click', changeBg, {
+    once: true
+});
