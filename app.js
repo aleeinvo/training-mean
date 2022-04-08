e008460478b1ee715b9f96cf3e0ae979357fdd47
aleeinvo <ali.asghar@invozone.com>
2022-04-08 20:10:03 +0500

    print the page

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-let value = null;
+let print = document.getElementById('print');
 
-value = void (4 + 5);
-
-console.log(value);
-
-// run this function immeditly
-void function something() {
-    console.log('something');
-}();
-
-let doSomething = document.getElementById('refresh');
-
-function doSomethingFunc() {
-    console.log('We are doing something');
-
-    return true;
-}
-
-doSomething.onclick = () => void doSomethingFunc();
+print.addEventListener('click', () => {
+    window.print();
+})
