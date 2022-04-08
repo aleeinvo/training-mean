5c72af2026d1c3f5ae0a49681d6360dd732dbfa0
aleeinvo <ali.asghar@invozone.com>
2022-04-08 14:48:43 +0500

    Remove the event listner

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const btn = document.getElementById('changeBg');
 
+let count = 0;
+
 function random(number) {
     return Math.floor(Math.random() * (number + 1));
 }
 
-btn.addEventListener('click', () => {
+function changeBg(){
     const randColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
 
     document.body.style.backgroundColor = randColor;
-});
+
+    count++;
+
+    if(count >= 10) {
+        btn.removeEventListener('mouseover', changeBg);
+    }
+}
+
+btn.addEventListener('mouseover', changeBg);
