20de4da6d5ad094525e05646b296bb1bd1d55777
aleeinvo <ali.asghar@invozone.com>
2022-04-11 08:48:07 +0500

    custom nested objects

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ function Car(name, model) {
     this.model = model;
 
     this.run = (speed = 120) => {
-        console.log(this.name +' is runing at ' + speed + '...........');
+        console.log(this.name +' is runing at ' + speed + ' by ' + this.owner);
     }
 }
 
+function Owner(name, city) {
+    this.name = name;
+    this.city = city;
+}
+
 let honda = new Car('Honda', 2008);
+honda.owner = new Owner('Alee', 'Lahore');
+console.log(honda);
 honda.run(180);
