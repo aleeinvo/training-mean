4a2adbe23dd45c5b363dfc7dc9f20044513e6224
aleeinvo <ali.asghar@invozone.com>
2022-04-12 02:48:01 +0500

    date constructor

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-// Arrays
+// Date
 
-let users = [
-    'alee',
-    'arslan',
-    'usman',
-    'numan',
-    'asher',
-];
+let now = Date();
 
-let numbers = [
-    1,
-    2,
-    3,
-    4,
-    5
-];
-
-const inventory = [
-    { name: 'asparagus', type: 'vegetables', quantity: 5 },
-    { name: 'bananas', type: 'fruit', quantity: 0 },
-    { name: 'goat', type: 'meat', quantity: 23 },
-    { name: 'cherries', type: 'fruit', quantity: 5 },
-    { name: 'fish', type: 'meat', quantity: 22 }
-];
-
-console.log(users.slice());
-
-console.log(users);
+console.log(now.toLocaleLowerCase());
