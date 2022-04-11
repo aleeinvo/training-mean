8c4495724acfc5ba83ae90449cecbd53ad7a6be3
aleeinvo <ali.asghar@invozone.com>
2022-04-12 03:09:06 +0500

    date get methods

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // Date
 
-let dob = new Date(1993, 8, 13);
+let dob = new Date(1993, 8, 13, 0, 0, 0, 0);
 
-console.log(dob.toJSON());
+console.log(dob.toString());
+console.log(dob.toUTCString());
+console.log(dob.toISOString());
+console.log(dob.toGMTString());
+console.log(dob.toLocaleString());
+console.log(dob.valueOf());
