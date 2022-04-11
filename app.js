4b9a95e55eda81576a96ae2c96135268cdf38c1d
aleeinvo <ali.asghar@invozone.com>
2022-04-11 08:21:46 +0500

    get all the props of object
    including the hidden one on the prototypes

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ data.name = 'Alee';
 data.city = 'Lahore';
 data.work = 'Dev';
 
-for(let prop of Object.keys(data)) {
-    console.log(`${prop} => ${data[prop]}`); 
+// Get the hidden props
+
+function getAllProps(o) {
+    let discover = o;
+    let props = [];
+
+    while(discover !== null) {
+        props = props.concat(Object.getOwnPropertyNames(discover));
+        discover = Object.getPrototypeOf(discover);
+    }
+
+    return props;
 }
+
+console.log(getAllProps(data));
