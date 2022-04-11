c5899e0022e6b0a217e6fa9db0bc4cb5ae7b9ce0
aleeinvo <ali.asghar@invozone.com>
2022-04-11 15:51:59 +0500

    array filter

--- app.js
+++ app.js
@@ -8,15 +8,6 @@ let users = [
     'asher'
 ];
 
-let moreUsers = [
-    'kirla',
-    'bhalo',
-    'khan',
-    'ubbaid'
-];
-
-let data = [1,2 ,3, 4, 5];
-
-data.fill(0, 1, 3);
-
-console.log(data);
+console.log(users.filter((el) => {
+    return el.startsWith('a');
+}))
