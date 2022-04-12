607418c632996cc8f04216c9a0d85223138b34d8
aleeinvo <ali.asghar@invozone.com>
2022-04-12 10:24:39 +0500

    Math function till min

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 // Math
 
-console.log(Math.abs(-45));
+console.log(Math.clz32(1));
+
+console.log(Math.exp(99));
+
+console.log(Math.expm1(99));
+
+console.log(Math.floor(4.989));
+
+console.log(Math.fround(4.889));
+
+console.log(Math.fround(1.2));
+
+console.log(Math.hypot(40, 2));
+
+console.log(Math.imul(3, 4));
+
+let numbers = [
+    34,
+    45,
+    98765,
+    23,
+    565,
+    3456,
+    7887
+];
+
+console.log(Math.max(...numbers));
+
+// or 
+
+let max = numbers.reduce((prev, current) => {
+    return Math.max(prev, current);
+}, -Infinity);
+
+let min = numbers.reduce((prev, current) => {
+    return Math.min(prev, current);
+}, Infinity);
+
+console.log(max);
