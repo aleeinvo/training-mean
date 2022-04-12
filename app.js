f82109ad9f3c2a770352ae8032acf051ab18d513
aleeinvo <ali.asghar@invozone.com>
2022-04-12 22:55:28 +0500

    regexp

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-// Math
+var singleLetterWord = /\b\w\b/g;
+var notSoLongWord = /\b\w{1,6}\b/g;
+var loooongWord = /\b\w{13,}\b/g;
 
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+var sentence = "Why do I have to learn multiplication table?";
 
-console.log(random(1, 10));
+console.table(sentence.match(singleLetterWord)); // ["I"]
+console.table(sentence.match(notSoLongWord));    // [ "Why", "do", "I", "have", "to", "learn", "table" ]
+console.table(sentence.match(loooongWord));      // ["multiplication"]
