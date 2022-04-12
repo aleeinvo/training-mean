bab331badaa3db734aec2bd000ed33ae15284b96
aleeinvo <ali.asghar@invozone.com>
2022-04-12 09:29:40 +0500

    Math abs

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
 // Math
 
-console.log('E',Math.E);
-console.log('LN2', Math.LN2);
-console.log('LN10', Math.LN10);
-console.log('LOG2E', Math.LOG2E);
-console.log('LOG10E', Math.LOG10E);
-console.log('PI', Math.PI);
-console.log('SQRT1_2', Math.SQRT1_2);
-console.log('SQRT2', Math.SQRT2);
+console.log(Math.abs(-45));
